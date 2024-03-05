2ebfbf05247e6c8c42757d373b08ac2ffee9fae9
Kader Nianmin SECK <seckkader41@gmail.com>
2024-03-05 16:23:29 +0000

    add the new image

--- index.js
+++ index.js
@@ -4,12 +4,6 @@ const PORT = 3000;
 
 app.use(express.static('public'));
 
-app.use((req, res, next) => {
-  const ip = req.ip || req.connection.remoteAddress;
-  console.log("Adresse IP du client:", ip);
-  next();
-});
-
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
