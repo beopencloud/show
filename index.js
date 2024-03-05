e4aef3095966c1d0221ae571d33e9e0ea5f5d26c
Kader Nianmin SECK <seckkader41@gmail.com>
2024-03-05 13:11:26 +0000

    add the b test

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const PORT = 3000;
 
 app.use(express.static('public'));
 
+app.use((req, res, next) => {
+  const ip = req.ip || req.connection.remoteAddress;
+  console.log("Adresse IP du client:", ip);
+  next();
+});
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
